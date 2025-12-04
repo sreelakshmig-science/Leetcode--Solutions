abadf918d5e5f83841a994dd3f31fc9ea832c0ba
Sreelakshmi G <145840226+sreelakshmig-science@users.noreply.github.com>
2025-12-04 23:43:58 +0000

    commented the second type of code

--- 292. Nim Game.py
+++ 292. Nim Game.py
@@ -1,8 +1,8 @@
 class Solution:
     def canWinNim(self, n: int) -> bool:
-          if(n%4==0): #the logic of the question is the condition should return False when 'n' is a multiple of 4
-             return False
-         else:
-             return True
-The same if else logic can be implemented in a single return statement. Here the space complexity is lower.
-          return(n%4!=0) 
+        if(n%4==0): #the logic of the question is the condition should return False when 'n' is a multiple of 4
+            return False
+        else:
+            return True
+#The same if else logic can be implemented in a single return statement. Here the space complexity is lower.
+          #return(n%4!=0) 
