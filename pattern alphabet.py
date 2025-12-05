72c25490bebd576ba254c72ab5ecbb4e026f1be7
Sreelakshmi G <145840226+sreelakshmig-science@users.noreply.github.com>
2025-12-05 07:05:24 +0000

    Patter code updated

---
+++ pattern alphabet.py
@@ -0,0 +1,24 @@
+n=int(input())
+for i in range (1, n+1):
+    for i in range(1,i+1):
+        a=chr((i%26)+96)
+        if (i%26==0):
+            print("z",end="")
+        else:
+            print(a,end="")
+    print()
+#The alphabet will be printed in "n" number of lines where each line will be updated with the alphabets.
+#After z "a" will be repeated
+
+n=int(input())
+j=n
+for i in range (1,n+1,2):
+    for x in range(1,j+1):
+        print(" "*j,end="")
+        j=j-1
+        break
+    for k in range (1,i+1):
+        print("*"*i,end="")
+        break
+    print()
+# Pyramid patter of *
