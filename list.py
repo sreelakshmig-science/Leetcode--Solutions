27b0808d80126fbd925378567a47a799bff44820
Sreelakshmi G <145840226+sreelakshmig-science@users.noreply.github.com>
2025-12-06 08:28:26 +0000

    Updated Diagonal Difference solution

--- list.py
+++ list.py
@@ -29,3 +29,14 @@ while (start<end):
     end-=1
 print(li)
 print(li[::-1])
+
+#Diagonal difference
+# li=[[1,2,3],[3,5,7],[9,10,11]]
+# d1=d2=0
+# for i in range(len(li)):
+#     for j in range(len(li[0])):
+#         if(i==j):
+#             d1+=li[i][j]
+#         if(i+j==len(li)-1):
+#             d2+=li[i][j]
+# print(abs(d1-d2))
