ae816a382c27a974838230961de36194ba70a058
Sreelakshmi G <145840226+sreelakshmig-science@users.noreply.github.com>
2025-12-06 05:02:47 +0000

    283. Move Zeroes solution updated

--- list.py
+++ list.py
@@ -8,3 +8,13 @@ for i in li[:]:
             li.remove(i)
         print(li)
 print(count)
+
+#move zeroes to the last leetcode-283
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        i=0
+        for j in range(len(nums)):
+            if (nums[j]!=0):
+                nums[i],nums[j]=nums[j],nums[i]
+                i+=1
+            
