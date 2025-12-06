8c98fa2642703f99005de96b1ae0b78777409d5b
Sreelakshmi G <145840226+sreelakshmig-science@users.noreply.github.com>
2025-12-06 05:16:56 +0000

    reverse a list without build-in function solution updated

--- list.py
+++ list.py
@@ -17,4 +17,15 @@ class Solution:
             if (nums[j]!=0):
                 nums[i],nums[j]=nums[j],nums[i]
                 i+=1
-            
+#reverse a list without build-in function
+li=[1,2,3,4,5]
+for i in range(len(li)//2):
+    li[i], li[len(li)-i-1]=li[len(li)-i-1],li[i]
+start=0
+end=len(li)-1
+while (start<end):
+    li[start],li[end]=li[end],li[start]
+    start+=1
+    end-=1
+print(li)
+print(li[::-1])
