e96728d983942f93adfa3babdebb264983e460e4
Sreelakshmi G <145840226+sreelakshmig-science@users.noreply.github.com>
2025-12-11 06:01:04 +0530

    Correct containsDuplicate function implementation
    Fix syntax errors and improve function logic.

--- 217. Contains Duplicate.py
+++ 217. Contains Duplicate.py
@@ -3,4 +3,12 @@ class solution:
     if (leng(set(nums)==len(nums)):
       return false
     else:
-      return True
+      return True #4ms
+
+or
+
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        if (len(set(nums))==len(nums)):
+            return False
+        return True #0ms
