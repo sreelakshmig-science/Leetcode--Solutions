a2d2b4782ed09133668a073bbdedf7b168099439
Sreelakshmi G <145840226+sreelakshmig-science@users.noreply.github.com>
2026-04-24 20:36:59 +0530

    Update 217. Contains Duplicate.py

--- 217. Contains Duplicate.py
+++ 217. Contains Duplicate.py
@@ -1,14 +1,14 @@
-class solution:
-  def containsDuplicate(self,nums: List[int])->bool:
-    if (leng(set(nums)==len(nums)):
-      return false
-    else:
-      return True #4ms
-
-or
-
-class Solution:
-    def containsDuplicate(self, nums: List[int]) -> bool:
-        if (len(set(nums))==len(nums)):
-            return False
-        return True #0ms
+  class solution:
+    def containsDuplicate(self,nums: List[int])->bool:
+      if (leng(set(nums)==len(nums)):
+        return false
+      else:
+        return True #4ms
+  
+  or
+  
+  class Solution:
+      def containsDuplicate(self, nums: List[int]) -> bool:
+          if (len(set(nums))==len(nums)):
+              return False
+          return True #0ms
