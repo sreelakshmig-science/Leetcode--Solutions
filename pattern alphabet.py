1fee81d8492694db8ba32bbfdf0bd664738975c6
Sreelakshmi G <145840226+sreelakshmig-science@users.noreply.github.com>
2026-04-29 20:04:46 +0530

    Update pattern alphabet.py

--- pattern alphabet.py
+++ pattern alphabet.py
@@ -10,18 +10,18 @@
   # #The alphabet will be printed in "n" number of lines where each line will be updated with the alphabets.
   # #After z "a" will be repeated
 
-# # n=int(input())
-# # j=n
-# # for i in range (1,n+1,2):
-# #     for x in range(1,j+1):
-# #         print(" "*j,end="")
-# #         j=j-1
-# #         break
-# #     for k in range (1,i+1):
-# #         print("*"*i,end="")
-# #         break
-# #     print()
-# # # Pyramid patter of *
+  # # n=int(input())
+  # # j=n
+  # # for i in range (1,n+1,2):
+  # #     for x in range(1,j+1):
+  # #         print(" "*j,end="")
+  # #         j=j-1
+  # #         break
+  # #     for k in range (1,i+1):
+  # #         print("*"*i,end="")
+  # #         break
+  # #     print()
+  # # # Pyramid patter of *
 
 # n=int(input())
 # x=1
