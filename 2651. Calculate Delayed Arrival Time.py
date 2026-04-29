05aefb889d4a55293109208647dfee84eb677e3d
Sreelakshmi G <145840226+sreelakshmig-science@users.noreply.github.com>
2026-04-29 19:59:24 +0530

    Update 2651. Calculate Delayed Arrival Time.py

--- 2651. Calculate Delayed Arrival Time.py
+++ 2651. Calculate Delayed Arrival Time.py
@@ -1,3 +1,3 @@
-class Solution:
-    def findDelayedArrivalTime(self, x: int, y: int) -> int:
-        return (x+y)%24 #the 24 hour cycle repeats.
+    class Solution:
+        def findDelayedArrivalTime(self, x: int, y: int) -> int:
+            return (x+y)%24 #the 24 hour cycle repeats.
